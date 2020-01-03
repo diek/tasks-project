611c80a3290f83fcf14c1e645c8fdffde07bcf3e
diek <d_kearney@bellaliant.net>
2020-01-02 23:55:27 -0300

    Add custom text to Admin

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = 'Tasks Admin Panel'
+admin.site.site_title = 'Tasks - Admin'
+
 urlpatterns = [
     path('tasks/', include('tasks.urls')),
     path('admin/', admin.site.urls),
